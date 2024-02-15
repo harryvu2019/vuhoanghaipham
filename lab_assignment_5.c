011507ecc95fc5439c054332f7a1f30083567ae0
harryvu2019 <harryvu2019@gmail.com>
2024-02-15 17:03:41 -0500

    initial commit

--- lab_assignment_5.c
+++ lab_assignment_5.c
@@ -1,76 +1,125 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node {
-	char letter;
-	struct node* next;
+typedef struct node
+{
+    char letter;
+    struct node *next;
 } node;
 
 // Returns number of nodes in the linkedList.
-int length(node* head)
+
+int length(node *head)
+// In order to read the length, we have to measure its length by reading thru it => a new variable
 {
-   struct node *tmp = head;
-    int len = 0;
-   while (tmp != NULL)
-   {
-      tmp = tmp->next;
-      len++;
-   }
-
-   return (len);
+    int count = 0;
+    // The linked list is traversed from head to the last node, and length is increased at each iteration.
+    while (head != NULL)
+    {
+        count++;
+        head = head->next; // keep traversing
+    }
+    return count; // return the length of that linkedlist
 }
-
 // parses the string in the linkedList
 //  if the linked list is head -> |a|->|b|->|c|
 //  then toCString function wil return "abc"
-char* toCString(node* head)
+char *toCString(node *head)
 {
+    int sizeOfString = length(head);
+    int i = 0;
+    char *string = (char *) malloc(sizeOfString + 1);
+	if (head == NULL){
+		printf(" The string is empty");
+	}
+    // traversing and add head->letter to the string above
+    while (head != NULL)
+    {
+        string[i] = head->letter;
+        head = head->next;
+        i++; // increase counter variable
+    }
+    string[sizeOfString] = '\0'; // set the last char = NUL
+    return string;
 }
 
 // inserts character to the linkedlist
 // f the linked list is head -> |a|->|b|->|c|
 // then insertChar(&head, 'x') will update the linked list as foolows:
 // head -> |a|->|b|->|c|->|x|
-void insertChar(node** pHead, char c)
+void insertChar(node **pHead, char c)
 {
+    // initialize a new Node to store char c
+    node *newNode = (node *) malloc(sizeof(node));
+    //check the condition
+    if(newNode == NULL)
+    {
+        printf("Memory Allocation failure");
+    }
+    newNode->letter = c;
+    newNode->next = NULL;
+    // a temp pointer 4 traversing purpose
+    node *temp = *pHead;
+    // check the condition if pHead = NULL -> add char c to head
+    if (*pHead == NULL)
+    {
+        *pHead = newNode;
+        return;
+    }
+    else
+    {
+        // Add newNode to the last node
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
 }
-
 // deletes all nodes in the linkedList.
-void deleteList(node** pHead)
+void deleteList(node **pHead)
 {
+    // initialize a temp pointer for traversing purpose
+    node *temp = *pHead;
+    while (temp != NULL)
+    {
+        temp = temp->next;
+        free(*pHead);
+        *pHead = temp;
+    }
 }
 
 int main(void)
 {
-	int i, strLen, numInputs;
-	node* head = NULL;
-	char* str;
-	char c;
-	FILE* inFile = fopen("input.txt","r");
+    int i, strLen, numInputs;
+    node *head = NULL;
+    char *str;
+    char c;
+    FILE *inFile = fopen("input.txt", "r"); // open file for reading
 
-	fscanf(inFile, " %d\n", &numInputs);
-	
-	while (numInputs-- > 0)
-	{
-		fscanf(inFile, " %d\n", &strLen);
-		for (i = 0; i < strLen; i++)
-		{
-			fscanf(inFile," %c", &c);
-			insertChar(&head, c);
-		}
+    fscanf(inFile, " %d\n", &numInputs); // reads the numbers of inputs from the file, consuming any leading whitespace characters
 
-		str = toCString(head);
-		printf("String is : %s\n", str);
+    while (numInputs-- > 0)
+    {
+        fscanf(inFile, " %d\n", &strLen);
+        for (i = 0; i < strLen; i++)
+        {
+            fscanf(inFile, " %c", &c);
+            insertChar(&head, c);
+        }
 
-		free(str);
-		deleteList(&head);
+        str = toCString(head);
+        printf("String is : %s\n", str);
 
-		if (head != NULL)
-		{
-			printf("deleteList is not correct!");
-			break;
-		}
-	}
+        free(str);
+        deleteList(&head);
 
-	fclose(inFile);
+        if (head != NULL)
+        {
+            printf("deleteList is not correct!");
+            break;
+        }
+    }
+
+    fclose(inFile);
 }
